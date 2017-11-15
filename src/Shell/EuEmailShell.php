5b0451c998bd530fdaacbf6f3d1739a820ad4204
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2017-11-15 12:45:56 +0100

    created a not formated email in a shell issue #4

--- src/Shell/EuEmailShell.php
+++ src/Shell/EuEmailShell.php
@@ -7,15 +7,38 @@
  */
 
 namespace App\Shell;
+
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Log\Log;
+use Cake\Mailer\Email;
 
 
-class EuEmaliShell extends Shell
+class EuEmailShell extends Shell
 {
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Bhaktas');
+    }
+
     public function main()
     {
-        $this->out('Hello World');
+        $bhaktas = $this->Bhaktas->findEuCardExperiendInCurrentMonth()->select([
+            'Bhaktas.id',
+            'Bhaktas.nev_szuletesi',
+            'Bhaktas.nev_polgari',
+            'Bhaktas.eu_card_expiry'
+        ])->all();
+        $message = '';
+        foreach ($bhaktas as $bhakta) {
+            $message .= 'id: ' . $bhakta->id . ' nev_szuletesi: ' . $bhakta->nev_szuletesi . ' nev_polgari ' . $bhakta->nev_polgary . ' eu_card_expiry' . $bhakta->eu_card_expiry . '<br>';
+        }
+        debug($message);
+        $email = new Email('default');
+        $email->setSubject('bhaktas with experied eu card')
+            ->setTo('proba@proba.hu');
+        $email->send($message);
     }
 }
