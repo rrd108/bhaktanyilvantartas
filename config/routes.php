cff51207d5b6a15a7855afea8b5f24a98febca00
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2017-11-15 15:30:55 +0100

    removed endvolunteer route from routes.php

--- config/routes.php
+++ config/routes.php
@@ -51,8 +51,6 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
-    $routes->connect('/bhaktas/endvolunteer/*', ['controller' => 'Bhaktas', 'action' => 'endVolunteer']);
-
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
