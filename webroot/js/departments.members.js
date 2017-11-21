fae6653bd1cbb9c5e1f1fbed6037ebf02fd83fbe
Unknown <ifjuszalaimihaly@gmail.com>
2017-11-21 17:50:53 +0100

    can replace the intems in the view, departments.members.js, issue #16

---
+++ webroot/js/departments.members.js
@@ -0,0 +1,16 @@
+var sourceListId;
+var targetListId;
+$(".drag").draggable({
+    drag: function (i, el) {
+        sourceListId = $(this).parent().attr("id");
+    }
+});
+$(".drop").each(function (i, el) {
+    $(this).droppable({
+        drop: function (event, ui) {
+            targetListId = $(this).attr("id");
+            var itemId = ui.draggable.attr("id");
+            $("#" + itemId).appendTo("#" + targetListId);
+        }
+    });
+});
